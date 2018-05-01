632a5a154c98c47e28c94ffaed6a4cc3377545ac
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-05-01 20:55:07 +0530

    added functionality to search username via input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
 import {Button} from './Components/button';
-
-
+import {Input} from './Components/input';
 
 
 class App extends Component {
@@ -11,20 +11,46 @@ class App extends Component {
   super(props);
 
   this.state = {
-    data: ''
+    username: '',
+    avatar_url:''
   }
  }
 
 
  handleClick(){
-  console.log('zu-zu')
+
+  let url_ = "https://api.github.com/users/"+ this.state.username;
+  console.log(url_);
+  axios.get(url_)
+    .then(response => response.data.avatar_url)
+    .then((avatar_url) => {
+      this.setState({
+        avatar_url: avatar_url
+      })
+    })
  }
 
+ handleChange(event){
+  this.setState({
+    username:event.target.value
+  })
+ }
 
   render() {
+
+    let avatar_;
+    if(this.state.avatar_url != '')
+           avatar_ =  <img src={this.state.avatar_url}  className = "_img img-responsive" alt="Smiley face" />
+      else {
+           avatar_ = '';
+      }
+ 
     return (
       <div className="App">
+
+       <Input value = {this.state.username} onChange = {(e) => {this.handleChange(e)}}/>
        <Button  onClick = {(e)=> {this.handleClick(e)}}/>
+        {avatar_}
       </div>
     );
   }
---
+++ src/Components/input.js
@@ -0,0 +1,11 @@
+
+
+
+import React from 'react';
+import ReactDom from 'react-dom';
+
+export const Input = (props) => {
+	return(
+			<input className  = "form-control _wid" onChange = {props.onChange} /> 
+	)
+}
