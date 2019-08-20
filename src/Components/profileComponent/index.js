408199b719bd67513e65f43eef3216dcba5cf721
Divyanshu <divyanshu.r46956@gmail.com>
2019-08-21 01:37:01 +0200

    Added new font family 👋

--- src/Components/profileComponent/index.js
+++ src/Components/profileComponent/index.js
@@ -7,7 +7,8 @@ export default class Profile extends React.Component {
     console.log("data", this.props);
   }
   render() {
-    const { isFetching } = this.props.state.USER_PROFILE_REDUCER.data;
+    const { isFetching } = this.props.state.USER_PROFILE_REDUCER;
+    const { data } = this.props.state.USER_PROFILE_REDUCER;
     if (isFetching) {
       return (
         <div>
@@ -18,8 +19,22 @@ export default class Profile extends React.Component {
     return (
       <div>
         <div className="flex flex-row m-t-2em pd-2em black-border">
-          <div className="flex flex-col black-border" />
-          <div className="flex flex-col black-border" />
+          <div className="flex flex-col black-border pd-2em " >
+            <div className="username-profile-picture-wrapper flex flex-row">
+                <div className="profile-picture-wrapper">
+                      <img src={data.avatar_url} className="profile-picture"></img>
+                </div>
+                <div className="m-l-1em username-wrapper">
+                    <p className="username">{data.name}</p>
+                    <p className="portfolio">{data.blog}</p>
+                </div>
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="flex flex-col black-border">
+          </div>
         </div>
       </div>
     );
