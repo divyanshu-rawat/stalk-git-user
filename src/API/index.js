e8a0850156cd8e58e6253c181e8cffddc3b04740
Divyanshu <divyanshu.r46956@gmail.com>
2019-08-22 01:08:07 +0200

    Implemented Parallel calls to api using axios.all ★

--- src/API/index.js
+++ src/API/index.js
@@ -1,11 +1,31 @@
 import axios from "axios";
 
-export async function getApiData(username) {
-  console.log("username", username);
-  const URL = `https://api.github.com/users/${username}`;
+export async function getApiData(name) {
   try {
-    const { data } = await axios(URL);
-    return data;
+    return await axios
+      .all([
+        axios.get("https://api.github.com/users/" + name),
+        axios.get(
+          "https://api.github.com/users/" + name + "/events?per_page=" + 20
+        ),
+        axios.get(
+          "https://api.github.com/users/" + name + "/repos?per_page=100"
+        )
+      ])
+      .then(
+        axios.spread(
+          (userProfileInformation, userActivityStatstics, userRepositories) => {
+            return {
+              userProfileInformation: userProfileInformation.data,
+              userActivityStatstics: userActivityStatstics.data,
+              userRepositories: userRepositories.data
+            };
+          }
+        )
+      )
+      .catch(err => {
+        console.log(err);
+      });
   } catch (err) {
     return new Promise(reject => reject(err));
   }
--- src/Components/profileComponent/index.js
+++ src/Components/profileComponent/index.js
@@ -4,11 +4,11 @@ import loader from "../../Assets/loader.gif";
 export default class Profile extends React.Component {
   constructor(props) {
     super(props);
-    console.log("data", this.props);
+    console.log("profielComponent", this.props);
   }
   render() {
     const { isFetching } = this.props.state.USER_PROFILE_REDUCER;
-    const { data } = this.props.state.USER_PROFILE_REDUCER;
+    const { userProfileInformation } = this.props.state.USER_PROFILE_REDUCER.data;
     if (isFetching) {
       return (
         <div>
@@ -22,25 +22,25 @@ export default class Profile extends React.Component {
           <div className="flex flex-col black-border pd-2em ">
             <div className="username-profile-picture-wrapper flex flex-row">
               <div className="profile-picture-wrapper">
-                <img src={data.avatar_url} className="profile-picture" />
+                <img src={userProfileInformation.avatar_url} className="profile-picture" />
               </div>
               <div className="m-l-1em username-wrapper">
-                <p className="username">{data.name}</p>
-                <p className="portfolio">{data.blog}</p>
+                <p className="username">{userProfileInformation.name}</p>
+                <p className="portfolio">{userProfileInformation.blog}</p>
               </div>
             </div>
             <div className="git-statistics">
               <div className="stats-list-item">
                 <h3 className="stats-list-item-heading">Followers</h3>
-                <p className="stats-list-item-paragraph">{data.followers}</p>
+                <p className="stats-list-item-paragraph">{userProfileInformation.followers}</p>
               </div>
               <div className="stats-list-item">
                 <h3 className="stats-list-item-heading">Following</h3>
-                <p className="stats-list-item-paragraph">{data.following}</p>
+                <p className="stats-list-item-paragraph">{userProfileInformation.following}</p>
               </div>
               <div className="stats-list-item">
                 <h3 className="stats-list-item-heading">public Repositories</h3>
-                <p className="stats-list-item-paragraph">{data.public_repos}</p>
+                <p className="stats-list-item-paragraph">{userProfileInformation.public_repos}</p>
               </div>
             </div>
             <div />
